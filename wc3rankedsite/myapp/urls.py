1599fb6f586920c825b5433aa8c716206347332c
youssef.ouchouid <youssef.ouchouid@intempora.com>
2024-01-29 19:48:38 +0100

    leaderboards quasi fonctionnel à 100% manque 2 trucs

--- wc3rankedsite/myapp/urls.py
+++ wc3rankedsite/myapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('leaderboards/', leaderboards, name='leaderboards'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
-    path('api/leaderboards/', get_filtered_leaderboard, name='get_filtered_leaderboard')
+    path('api/leaderboards/', get_filtered_leaderboard, name='get_filtered_leaderboard'),
+    path('get_total_count/', get_total_count, name='get_total_count')
 ]
--- wc3rankedsite/myapp/views.py
+++ wc3rankedsite/myapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from .models import Book
 from .models import Entry
 from .models import GameSetting
+from django.utils import timezone
 from django.http import JsonResponse
 
 def book_list(request):
@@ -17,7 +18,7 @@ def leaderboards(request):
     # Retrieve unique values for Season, Gamemode, and Race
     seasons = GameSetting.objects.values_list('season', flat=True).distinct()
     gamemodes = GameSetting.objects.values_list('gamemode', flat=True).distinct()
-    races = GameSetting.objects.values_list('race', flat=True).distinct()
+    races = ["All", "Human", "Orc", "Night Elf", "Undead", "Random"]
 
     context = {
         'seasons': seasons,
@@ -42,12 +43,22 @@ def get_filtered_leaderboard(request):
     range_param = request.GET.get('range', '0-100')
     start, end = map(int, range_param.split('-'))
 
-    # Perform filtering based on the selected criteria and range
-    filtered_entries = Entry.objects.filter(
-        game_setting__season=season,
-        game_setting__gamemode=gamemode,
-        game_setting__race=race
-    )[start:end]
+    if race != 'all':
+        # Perform filtering based on the selected criteria and range
+        filtered_entries = Entry.objects.filter(
+            game_setting__season=season,
+            game_setting__gamemode=gamemode
+        )
+
+        filtered_entries = [entry for entry in filtered_entries if entry.race == race][start:end]
+
+    else:
+        # Perform filtering based on the selected criteria and range
+        filtered_entries = Entry.objects.filter(
+            game_setting__season=season,
+            game_setting__gamemode=gamemode,
+            game_setting__race=race
+        )[start:end]        
 
     # Serialize the filtered data
     serialized_data = [
@@ -67,3 +78,39 @@ def get_filtered_leaderboard(request):
     ]
 
     return JsonResponse(serialized_data, safe=False)
+
+def get_total_count(request):
+    season = request.GET.get('season')
+    gamemode = request.GET.get('gamemode')
+    race = request.GET.get('race')
+
+    # Add any additional filters you need
+
+    if race != "all":
+        # Perform filtering based on the selected criteria and range
+        filtered_entries = Entry.objects.filter(
+            game_setting__season=season,
+            game_setting__gamemode=gamemode
+        )
+
+        total_count = len([entry for entry in filtered_entries if entry.race == race])
+
+    else:
+        # Perform filtering based on the selected criteria and range
+        total_count = Entry.objects.filter(
+            game_setting__season=season,
+            game_setting__gamemode=gamemode,
+            game_setting__race=race
+        ).count()
+
+    # Fetch dataset datetime
+    dataset_datetime = GameSetting.objects.filter(season=season, gamemode=gamemode).latest('created_at').created_at
+    dataset_datetime_str = timezone.localtime(dataset_datetime).strftime('%Y-%m-%d %H:%M:%S')
+
+    # Include dataset datetime in the response
+    response_data = {
+        'total_count': total_count,
+        'dataset_datetime': dataset_datetime_str,
+    }
+
+    return JsonResponse(response_data)
